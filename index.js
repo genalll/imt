72a07711456feda48d60230e964e2e44ad66f4f7
genalll <genalll@mail.ru>
2019-12-23 14:40:32 +0500

    no commit

--- index.js
+++ index.js
@@ -52,4 +52,5 @@ document.getElementById("customRange1").addEventListener("change", function() {
   moreinfo.onclick = function() {
     document.location.href = "https://ru.wikipedia.org/wiki/%D0%98%D0%BD%D0%B4%D0%B5%D0%BA%D1%81_%D0%BC%D0%B0%D1%81%D1%81%D1%8B_%D1%82%D0%B5%D0%BB%D0%B0";
     
+    
   };
